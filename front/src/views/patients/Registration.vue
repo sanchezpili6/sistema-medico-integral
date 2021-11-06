e2538cb780926ae149f27e5a6f228d1a5950b37f
Jacqueline Andres <jacky.andres@hotmail.com>
2021-11-05 20:28:27 -0600

    Exceptions handler

--- front/src/views/patients/Registration.vue
+++ front/src/views/patients/Registration.vue
@@ -5,6 +5,14 @@
     </v-app-bar>
     <v-row class="text-center">
       <v-col class="mb-4 pa-15">
+        <v-dialog v-model="dialog" width="500">
+          <v-card>
+            <v-card-title>Faltan datos</v-card-title>
+            <v-card-text>
+              Por favor asegúrese de llenar todos los campos y que su nss no esté ya registrado
+            </v-card-text>
+          </v-card>
+        </v-dialog>
         <v-card class="mx-auto my-10" max-width="600">
           <v-card-title>REGISTRO PACIENTE</v-card-title>
           <v-row>
@@ -49,6 +57,7 @@
 </template>
 
 <script>
+let API_URL= 'https://c355-2806-2f0-9000-f884-3c43-128c-f17d-cfb9.ngrok.io';
 export default {
   name: "Registration",
   data: () => ({
@@ -56,13 +65,43 @@ export default {
     nombre: '',
     nss: '',
     poliza: '',
+    postId: -1,
+    dialog: false,
   }),
   computed:{
 
   },
   methods:{
-    register:function(){
-      console.log(this.nombre+" "+ this.apellidos+" "+this.nss+" "+ this.poliza);
+    async register(){
+      if(this.nombre!='' && this.apellidos!='' && this.nss && this.poliza){
+        this.dialog=false;
+        // POST request using fetch with async/await
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({name: this.nombre, last_name: this.apellidos, nss: this.nss, policy:this.poliza})
+        };
+        console.log(requestOptions.body);
+        /*const response = await fetch(API_URL+"/api/patient/create", requestOptions).then(async response => {
+          const data = await response.json();
+          // check for error response
+          if (!response.ok) {
+            // get error message from body or default to response status
+            const error = (data && data.message) || response.status;
+            return Promise.reject(error);
+          }
+
+          this.postId = data.id;
+        }).catch(error => {
+              this.errorMessage = error;
+              console.error('There was an error!', error);
+            });*/
+
+        console.log(this.nombre+" "+ this.apellidos+" "+this.nss+" "+ this.poliza);
+      }
+      else{
+        this.dialog=true;
+      }
     }
   }
 }
